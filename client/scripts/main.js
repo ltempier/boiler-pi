c40c93dac1c2006179de5c056d61df0cea2a5b09
a <z>
2014-11-10 18:58:16 +0100

    first display chart fn

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -8,8 +8,108 @@ $(document).ready(function () {
         var dateFrom = $('#dateFrom').datetimepicker('getUTCDate').getTime();
         var dateTo = $('#dateTo').datetimepicker('getUTCDate').getTime();
         var url = '/api/conso/' + dateFrom + '/' + dateTo;
-        $.get(url, function(){
-
-        })
+        $.ajax({
+            url: url,
+            success: function (data) {
+                display(data)
+            },
+            error: function (err) {
+                alert('erreur ' + JSON.stringify(err, null, 4))
+            }
+        });
     });
+
+    Highcharts.setOptions({
+        global : {
+            useUTC : false
+        }
+    });
+
+    fetchDay();
+    function fetchDay() {
+        var dayStart = moment().startOf('day');
+        var dayEnd = moment().endOf('day');
+        var dayUrl = '/api/conso/' + dayStart.valueOf() + '/' + dayEnd.valueOf();
+        $.get(dayUrl, function (dayData) {
+            display(dayData)
+        })
+    }
+    function display(data){
+        $('#chart').highcharts({
+            chart: {
+                type: 'area'
+            },
+            title: {
+                text: '',
+                style: {
+                    display: 'none'
+                }
+            },
+            subtitle: {
+                text: '',
+                style: {
+                    display: 'none'
+                }
+            },
+            xAxis: {
+                type: 'datetime',
+                dateTimeLabelFormats: {
+                    month: '%e. %b',
+                    year: '%b'
+                }
+            },
+            yAxis: {
+                plotLines: [
+                    {
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }
+                ]
+            },
+            plotOptions: {
+                area: {
+                    pointStart: 1940,
+                    marker: {
+                        enabled: false,
+                        symbol: 'circle',
+                        radius: 2,
+                        states: {
+                            hover: {
+                                enabled: true
+                            }
+                        }
+                    }
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [
+                {
+                    name: 'LED',
+                    data: formatData(data)
+                }
+            ]
+        });
+
+    }
+
+    function formatData(datas) {
+        var oldState = null;
+        var buffer = [];
+        var valueOn = 1;
+        var valueOff = 0;
+        datas.reverse()
+        _.each(datas, function (data) {
+            if ('null' != oldState)
+                buffer.push([moment(data.date).subtract(1, 'seconds').valueOf(), oldState ? valueOn : valueOff])
+            buffer.push([moment(data.date).add(0, 'seconds').valueOf(), data.state ? valueOn : valueOff])
+            oldState = data.state
+        });
+        return buffer
+    }
 });
--- server/api.js
+++ server/api.js
@@ -6,9 +6,19 @@ var dataCollection = require('./mongodb').collection('data');
 module.exports = function (app) {
     app.route('/api/conso/:from/:to')
         .get(function (req, res) {
-            var dateFrom = req.param('from');
-            var dateTo = req.param('to');
-
+            var dateFrom = new Date(parseInt(req.param('from')));
+            var dateTo = new Date(parseInt(req.param('to')) || Date.now());
+            var q = {"date": {"$gte": dateFrom, "$lt": dateTo}};
+            console.log(q);
+            dataCollection.find({ $query: q, $orderby: { date : -1 } })
+                .limit(1000)
+                .toArray(function (err, replies) {
+                    console.log(arguments)
+                    if (err)
+                        res.send(500);
+                    else
+                        res.json(replies)
+                })
         })
 };
 
