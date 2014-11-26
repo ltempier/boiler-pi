c80e88767027d9e223d0a8f3f029ded1d022d785
ltempier <laurent.tempier@gmail.com>
2014-11-26 23:53:32 +0100

    init alnar

--- client/app.js
+++ client/app.js
@@ -1,16 +1,44 @@
 var app = angular.module('app', ['ui.bootstrap', 'ngRoute']);
 
+
+var routes = [
+    {
+        title: 'Dashboard',
+        url: '/',
+        templateUrl: 'templates/graph.html',
+        controller: 'graph'
+    },
+    {
+        title: 'Planning',
+        url: '/planning',
+        templateUrl: 'templates/planning.html',
+        controller: 'planning'
+    },
+    {
+        title: 'Panel',
+        url: '/panel',
+        templateUrl: 'templates/panel.html',
+        controller: 'panel'
+    }
+];
+
 app.config(['$routeProvider', '$locationProvider',
     function ($routeProvider, $locationProvider) {
 
-        $routeProvider.when('/', {
-            templateUrl: 'templates/graph.html',
-            controller: 'graph'});
-        $routeProvider.when('/panel', {
-            templateUrl: 'templates/panel.html',
-            controller: 'panel'});
+        routes.forEach(function (route) {
+            $routeProvider.when(route.url, {
+                templateUrl: route.templateUrl,
+                controller: route.controller});
+        });
 
         $routeProvider.otherwise({redirectTo: '/'});
         $locationProvider.html5Mode(true);
     }
 ]);
+
+app.controller('navbar', ['$scope', '$location', function ($scope, $location) {
+    $scope.links = routes;
+    $scope.isActive = function (link) {
+        return link.url === $location.path();
+    }
+}]);
