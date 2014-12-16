d87cbf77bc6900b1357373aa817a1a1727b45587
ltempier <laurent.tempier@gmail.com>
2014-12-16 14:06:03 +0100

    init new page + add records script

--- client/scripts/dashboard.controller.js
+++ client/scripts/dashboard.controller.js
@@ -3,5 +3,4 @@ app.controller('dashboardCtrl', ['$scope', function ($scope) {
 
 
 
-
 }]);
---
+++ test/records.js
@@ -0,0 +1,48 @@
+var _ = require('underscore');
+var async = require('async');
+var moment = require('moment');
+
+require('../server/nedb').init(function (err) {
+    if (err) {
+        console.log('nedb.init() error');
+        return
+    }
+
+    var records = require('../server/nedb').get('records', true);
+    addRandomRecords(
+        moment().utc().startOf('year').valueOf(),
+        moment().utc().endOf('month').valueOf(),
+        function () {
+            console.log('addRandomRecords finish ', arguments)
+        });
+
+    function addRandomRecords(startDate, endDate, callback) {
+        var state = true;
+        var recordDate = startDate;
+        var count = 0;
+        async.whilst(
+            function () {
+                return recordDate < endDate
+            },
+            function (next) {
+                records.insert(
+                    {date: recordDate, state: state}
+                    , function (err) {
+                        if (err)
+                            next(err);
+                        else {
+                            count++;
+                            recordDate += _.random(10 * 1000, 3600 * 60);
+                            state = !state;
+                            next()
+                        }
+                    });
+
+            }, function (err) {
+                if (err)
+                    callback(err);
+                else
+                    callback(null, count)
+            })
+    }
+});
